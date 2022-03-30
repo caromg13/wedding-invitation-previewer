8226034667766e5dd55fe31463c43cedeb03b417
caromg13 <caroline.boldcreative@gmail.com>
2022-03-30 12:22:27 +1100

    in progress

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+function changeTheme(prm) {
+    if (prm == "formal") {
+    document.getElementById('theme-design').classList.add("theme-formal");
+    document.getElementById('theme-design').classList.remove("theme-spring");
+    document.getElementById('theme-design').classList.add("theme-artdeco");
+    }
+}
