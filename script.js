e774ef866cf6b77e4bbd4147118f56db7da53619
caromg13 <caroline.boldcreative@gmail.com>
2022-03-31 16:57:10 +1100

    update 31/3 5pm

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 
+//Text Live Formal
+
+function liveTextFormal(formText, themeFormalLive) {
+    document.getElementById(formText).onkeyup = function() {
+    document.getElementById(themeFormalLive).textContent = this.value;
+    };
+}
+
+liveTextFormal("formText","themeFormalLive");
+  
+
+
+//Text Live Spring
+
+function liveTextSpring(formText, themeSpringLive) {
+    document.getElementById(formText).onkeyup = function() {
+    document.getElementById(themeSpringLive).textContent = this.value;
+    };
+}
+
+liveTextSpring("formText","themeSpringLive");
 
 
 
+//Text Live Art Deco
+
+function liveTextArtDeco(formText, themeArtDecoLive) {
+    document.getElementById(formText).onkeyup = function() {
+    document.getElementById(themeArtDecoLive).textContent = this.value;
+    };
+}
+
+liveTextArtDeco("formText","themeArtDecoLive");
 
 
 
-function changeTheme(prm) {
-    if (prm == "formal") {
-    document.getElementById('theme-design').classList.add("theme-formal");
-    document.getElementById('theme-design').classList.remove("theme-spring");
-    document.getElementById('theme-design').classList.add("theme-artdeco");
+  
+//Theme background change
+
+
+function changeTheme(param) {
+
+    let themeFormal = document.getElementById(`themeFormal`);
+
+    if (param == "formal") {
+       themeFormal.style.backgroundImage = 'url("images/WeddingByDesign_themes-01.jpg")';
     }
+
+     else if (param == "spring") {
+        themeFormal.style.backgroundImage = 'url("images/WeddingByDesign_themes-02.jpg")';
+    }
+
+    else if (param == "artDeco") {
+        themeFormal.style.backgroundImage = 'url("images/WeddingByDesign_themes-03.jpg")';
+     }
+
 }
